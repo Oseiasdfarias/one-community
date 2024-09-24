b8bf523329ed288cf975b1ba1503965642d2e026
oseiasdfarias <oseias.dfarias@gmail.com>
2024-09-24 18:12:47 -0300

    feat: implementado a estrutura do projeto usando o padrão MVC e add as dependências do Spring Boot necessárias

---
+++ one-community/src/main/java/br/com/one_community/main/OneCommunityApplication.java
@@ -0,0 +1,13 @@
+package br.com.one_community.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneCommunityApplication.class, args);
+	}
+
+}
---
+++ one-community/src/test/java/br/com/one_community/OneCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.one_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
