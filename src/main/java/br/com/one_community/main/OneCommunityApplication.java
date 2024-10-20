2d9bea293b0686adf1007554ed7eb318d973b304
oseiasdfarias <oseias.dfarias@gmail.com>
2024-10-19 23:42:06 -0300

    feat: criado a Entidade User.java

--- src/main/java/br/com/one_community/main/OneCommunityApplication.java
+++ src/main/java/br/com/one_community/main/OneCommunityApplication.java
@@ -3,11 +3,10 @@ package br.com.one_community.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "br.com.one_community")
 public class OneCommunityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OneCommunityApplication.class, args);
 	}
-
 }
